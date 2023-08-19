531432f98a9c6d21f3292db89e2f3763b75967fa
pokhrelgopal <pokhrelgopal27@gmail.com>
2023-08-19 21:11:32 +0545

    updated

---
+++ vfs_fonts.js
@@ -0,0 +1,11 @@
+pdfMake.fonts = {
+  TimesNewRoman: {
+    normal:
+      "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/TimesNewRoman/Times-New-Roman.ttf",
+    bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/TimesNewRoman/Times-New-Roman-Bold.ttf",
+    italics:
+      "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/TimesNewRoman/Times-New-Roman-Italic.ttf",
+    bolditalics:
+      "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/TimesNewRoman/Times-New-Roman-Bold-Italic.ttf",
+  },
+};
